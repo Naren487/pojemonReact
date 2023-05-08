ec22f7e19ecc5c48971afe5d6e6a9e617ddcb903
Narendra Kumar <naren.v487@gmail.com>
2023-05-08 10:04:12 +0530

    Bui;d

---
+++ precache-manifest.a53ff140f936d4f1bc3cd90f8e066c84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1daac748ae23e16123a",
+    "url": "/static/css/main.d5e2b93b.chunk.css"
+  },
+  {
+    "revision": "e1daac748ae23e16123a",
+    "url": "/static/js/main.e1daac74.chunk.js"
+  },
+  {
+    "revision": "09ea3d9923b8c05c198b",
+    "url": "/static/js/1.09ea3d99.chunk.js"
+  },
+  {
+    "revision": "c3e091cf89551b762d9f",
+    "url": "/static/css/2.04b51cca.chunk.css"
+  },
+  {
+    "revision": "c3e091cf89551b762d9f",
+    "url": "/static/js/2.c3e091cf.chunk.js"
+  },
+  {
+    "revision": "4b229bea0cdaf85f7889",
+    "url": "/static/js/runtime~main.4b229bea.js"
+  },
+  {
+    "revision": "6c43b4cb20215e4ed23d399b3f172d94",
+    "url": "/index.html"
+  }
+];
